88725bf6701bd2c99c10e59bfd28341938e33013
scamgi <scampini.giacomo@gmail.com>
2024-03-31 23:18:23 +0200

    html and css of the main app added

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,49 @@
-import './App.scss'
+import "./App.scss";
 
 function App() {
   return (
-    <div className='rosso'>Ciao mondo.</div>
-  )
+    <main>
+      <div className="container">
+        <div className="new-task">
+          <input type="text" placeholder="Add a new task" className="new-task__text" />
+          <button className="new-task__btn">+</button>
+        </div>
+        <div className="todo">
+          <h1 className="todo__title">Tasks to do - 4</h1>
+          <div className="todo__list">
+            <div className="todo__list__element">
+              <span className="todo__list__element__text">To study React fundamentals</span>
+              <span className="todo__list__element__done">v</span>
+              <span className="todo__list__element__delete">x</span>
+            </div>
+            <div className="todo__list__element">
+              <span className="todo__list__element__text">To study React fundamentals</span>
+              <span className="todo__list__element__done">v</span>
+              <span className="todo__list__element__delete">x</span>
+            </div>
+            <div className="todo__list__element">
+              <span className="todo__list__element__text">To study React fundamentals</span>
+              <span className="todo__list__element__done">v</span>
+              <span className="todo__list__element__delete">x</span>
+            </div>
+            <div className="todo__list__element">
+              <span className="todo__list__element__text">To study React fundamentals</span>
+              <span className="todo__list__element__done">v</span>
+              <span className="todo__list__element__delete">x</span>
+            </div>
+          </div>
+        </div>
+        <div className="done">
+          <h1 className="done__title">Done - 1</h1>
+          <div className="done__list">
+            <div className="done__list__element">
+              <span className="done__list__element__text">To study React fundamentals</span>
+            </div>
+          </div>
+        </div>
+      </div>
+    </main>
+  );
 }
 
-export default App
+export default App;
