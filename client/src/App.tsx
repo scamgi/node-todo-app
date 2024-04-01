5aaed63909bec5142538a746b33d3d0776bf90d9
scamgi <scampini.giacomo@gmail.com>
2024-04-01 16:22:20 +0200

    createNewTask added to the react app

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,19 +1,37 @@
 import { useEffect, useState } from "react";
 import "./App.scss";
+import { Task } from './utils';
 
 function App() {
   const [tasks, setTasks] = useState([{}]);
+  const [newTaskName, setNewTaskName] = useState("");
 
   useEffect(() => {
     fetch('http://localhost:5000/api/tasks', { method: 'GET' }).then(res => res.json()).then(data => setTasks(data));
   }, []);
 
+  async function createNewTask() {
+    try {
+      const newTask = { name: newTaskName, completed: false };
+      const response = await fetch('http://localhost:5000/api/tasks', {
+        method: 'POST',
+        body: JSON.stringify(newTask)
+      });
+      const newTaskWithId = await response.json();
+      console.log(newTaskWithId);
+      setNewTaskName('');
+      setTasks([...tasks, newTaskWithId]);
+    } catch (error) {
+      console.error(error);
+    }
+  }
+
   return (
     <main>
       <div className="container">
         <div className="new-task">
-          <input type="text" placeholder="Add a new task" className="new-task__text" />
-          <button className="new-task__btn">+</button>
+          <input type="text" placeholder="Add a new task" className="new-task__text" value={newTaskName} onChange={e => setNewTaskName(e.target.value)} />
+          <button className="new-task__btn" onClick={(e) => createNewTask()}>+</button>
         </div>
         <div className="todo">
           <h1 className="todo__title">Tasks to do - 4</h1>
